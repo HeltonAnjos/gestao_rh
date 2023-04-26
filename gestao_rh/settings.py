91b70e5f3da3d9655b9799fedfae7ba7096537ef
HeltonAnjos <heltonanjos0@gmail.com>
2023-04-26 12:24:16 -0300

    Criando a app funcionários

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'empresas',
+    'empresas.apps.EmpresasConfig',
+    'funcionario.apps.FuncionarioConfig',
 ]
 
 MIDDLEWARE = [

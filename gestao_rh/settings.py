f99e4e63ae2f8d8479d740855a1a60747f11521a
HeltonAnjos <heltonanjos0@gmail.com>
2023-05-25 15:31:47 -0300

    correção settings

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -116,10 +116,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
